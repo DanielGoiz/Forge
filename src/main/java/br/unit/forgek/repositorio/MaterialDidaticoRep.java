947e69b36e48219337637ecb2768fe9f98c3b1ba
Daniel <carlos.dgois@souunit.com.br>
2024-05-16 20:05:01 -0300

    letsgo

--- src/main/java/br/unit/forgek/repositorio/MaterialDidaticoRep.java
+++
@@ -1,8 +0,0 @@
-package br.unit.forgek.repositorio;
-
-import br.unit.forgek.modelo.Empresa;
-import br.unit.forgek.modelo.MaterialDidatico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialDidaticoRep extends JpaRepository<MaterialDidatico, Long> {
-}
