9771fea2f0133878cf76af3eb9be8c95e0eadf69
Ms-footsteps <112671751+Ms-footsteps@users.noreply.github.com>
2024-05-19 14:58:34 -0300

    Add files via upload

---
+++ src/main/java/br/unit/forgek/modelo/Mentor.java
@@ -0,0 +1,74 @@
+package br.unit.forgek.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+public class Mentor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank(message = "O nome do Mentor é obrigatório")
+    @Size(min = 2, message = "O nome do Mentor deve ter ao menos 2 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email deve ser válido")
+    private String email;
+
+    private String especialidade;
+
+
+    private String horarioDisponivel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getHorarioDisponivel() {
+        return horarioDisponivel;
+    }
+
+    public void setHorarioDisponivel(String horarioDisponivel) {
+        this.horarioDisponivel = horarioDisponivel;
+    }
+}
